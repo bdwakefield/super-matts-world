41fa994dbdbdf5cae5ef66da13b9f3e84d2f2a5f
anthony.joseph.greer <anthony.joseph.greer@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-03-12 00:11:44 +0000

    Finally have Jump blocks working for some initial testing. Walk into the fire sprites to test level jumps!

--- SuperMattsWorld/src/org/myname/flixeldemo/parsing/Level.java
+++ SuperMattsWorld/src/org/myname/flixeldemo/parsing/Level.java
@@ -10,7 +10,6 @@ import org.flixel.FlxCore;
 import org.flixel.FlxG;
 import org.flixel.FlxState;
 import org.myname.flixeldemo.Enemy;
-import org.myname.flixeldemo.JumpBlock;
 import org.myname.flixeldemo.Player;
 import org.myname.flixeldemo.R;
 
@@ -18,104 +17,76 @@ import flash.geom.Point;
 
 public class Level extends FlxState
 {
-	public static final String LABEL_START = "start";
-	public static final HashMap<Integer, Level> levelSaves= new HashMap<Integer, Level>();
+	public static final String DEFAULT_START_LABEL = "start";
+
+	private static final HashMap<Integer, Level> levelSaves = new HashMap<Integer, Level>();
+
+	/** The next level to be loaded upon SwitchState to this class.*/
+	public static int nextLevel = R.raw.lvl_jump_test;
+	/** The current level to already loaded in this class. */
+	public static int currentLevel = R.raw.lvl_jump_test;
+	/** The label to start the player at in this class. start if not set. */
+	public static String startLabel;
 
 	protected int defaultTexture = R.drawable.tech_tiles;
 	protected int background;
 	protected String name;
 	protected int width, height;
-
-	public static int nextLevel = R.raw.lvl_test;
-	public static int currentLevel = R.raw.lvl_test;
-
 	protected Player player;
-//	protected final ArrayList<Enemy> enemies = new ArrayList<Enemy>();
-//	protected final HashMap<String, Point> labels = new HashMap<String, Point>();
-//	//-- TODO needs a container Object for inner variables.
-//	protected final ArrayList<Object> jump = new ArrayList<Object>();
-//	protected final ArrayList<FlxBlock> movingBlocks = new ArrayList<FlxBlock>();
-//	protected final ArrayList<FlxBlock> stationaryBlocks = new ArrayList<FlxBlock>();
-//	protected final ArrayList<FlxBlock> hurtBlocks = new ArrayList<FlxBlock>();
-//	protected final ArrayList<FlxBlock> deathBlocks = new ArrayList<FlxBlock>();
-
 	protected ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 	protected HashMap<String, Point> labels = new HashMap<String, Point>();
 	//-- TODO needs a container Object for inner variables.
-	protected ArrayList<Object> jump = new ArrayList<Object>();
+	protected ArrayList<FlxBlock> jump = new ArrayList<FlxBlock>();
 	protected ArrayList<FlxBlock> movingBlocks = new ArrayList<FlxBlock>();
 	protected ArrayList<FlxBlock> stationaryBlocks = new ArrayList<FlxBlock>();
 	protected ArrayList<FlxBlock> hurtBlocks = new ArrayList<FlxBlock>();
 	protected ArrayList<FlxBlock> deathBlocks = new ArrayList<FlxBlock>();
 
 	public Level()
-	{		
-		if (!Level.levelSaves.containsKey(nextLevel)){
+	{
+		if (Level.levelSaves.containsKey(nextLevel))
+			//-- Copy the contents of the saved level DOOD!
+			this.copyContents(Level.levelSaves.get(nextLevel));
+		else
 			new LevelParser(this);
-	
-			for(Iterator<FlxBlock> it = movingBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			for(Iterator<FlxBlock> it = stationaryBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			for(Iterator<FlxBlock> it = hurtBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			for(Iterator<FlxBlock> it = deathBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			super.add(player);
-	
-			for(Iterator<Enemy> it = enemies.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			FlxG.follow(player, 2.5f);
-			FlxG.followAdjust(0.5f, 0.0f);
-			FlxG.followBounds(0, 0, this.width + 100, this.height + 100);
-		}else{
-			
-			//new LevelParser(this);
-			for(Iterator<FlxBlock> it = movingBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			for(Iterator<FlxBlock> it = stationaryBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			for(Iterator<FlxBlock> it = hurtBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			for(Iterator<FlxBlock> it = deathBlocks.iterator(); it.hasNext();)
-				super.add(it.next());
-			
-	
-			super.add(player);
-	
-			for(Iterator<Enemy> it = levelSaves.get(nextLevel).enemies.iterator(); it.hasNext();)
-				super.add(it.next());
-	
-			FlxG.follow(player, 2.5f);
-			FlxG.followAdjust(0.5f, 0.0f);
-			FlxG.followBounds(0, 0, this.width + 100, this.height + 100);
-			
-			player.x = 400;
-			player.y = 100;
-			
-		
-		}
+
+		this.addComponentsToLayer();
+		this.setCameraFollow();
+
+		/*
+		 * SET START POINT - Check to see if we provided a new start point for the player. This is
+		 * set in the Level.switchLevel function. If the specified point is not
+		 * found, then start them at the beginning of the level.s
+		 */
+		Point startPoint = labels.containsKey(startLabel) ? labels.get(startLabel) : labels.get(DEFAULT_START_LABEL);
+		this.player.x = startPoint.x;
+		this.player.y = startPoint.y;
+
+		//-- Save?
 		currentLevel = LevelParser.KEY_RESOURCE_ADDR.get(this.name);
 		levelSaves.put(currentLevel, this);
 	}
-	
-	
-	public static void switchLevel(String nextName, String nextLabel)
+
+	/**
+	 * TODO COMMENT ME ROGER
+	 * 
+	 * @param nextLevelName
+	 * @param nextLevelLabel
+	 */
+	public static void switchLevel(String nextLevelName, String nextLevelLabel)
 	{
-		nextLevel = LevelParser.KEY_RESOURCE_ADDR.get(nextName);
+		nextLevel = LevelParser.KEY_RESOURCE_ADDR.get(nextLevelName);
+
 		if (FlxG.state instanceof Level)
 			levelSaves.put(LevelParser.KEY_RESOURCE_ADDR.get(((Level)FlxG.state).name), ((Level)FlxG.state));
+
+		//-- need to know where to start when we swap the levels!
+		//   if this is not found we will default to "start"
+		Level.startLabel = nextLevelLabel;
+
 		FlxG.switchState(Level.class);
-		
 	}
+
 	@Override
 	public void update()
 	{
@@ -182,11 +153,70 @@ public class Level extends FlxState
 				((Enemy)object1).exists = false;
 				Level.switchLevel("lvl_test","");
 			}
-
-			
-		}
-		
-		
+		}		
 		);
+
+		/*
+		 * JUMP BLOCKS - have to trigger the overridden overlaps function.
+		 */
+		FlxG.overlapArrayList(jump, player);
+	}
+
+	/**
+	 * Adds Level compenents to the layer in the superclass FlxState.
+	 */
+	private void addComponentsToLayer()
+	{
+		for(Iterator<FlxBlock> it = movingBlocks.iterator(); it.hasNext();)
+			super.add(it.next());
+
+		for(Iterator<FlxBlock> it = stationaryBlocks.iterator(); it.hasNext();)
+			super.add(it.next());
+
+		for(Iterator<FlxBlock> it = hurtBlocks.iterator(); it.hasNext();)
+			super.add(it.next());
+
+		for(Iterator<FlxBlock> it = deathBlocks.iterator(); it.hasNext();)
+			super.add(it.next());
+
+		super.add(player);
+
+		for(Iterator<Enemy> it = enemies.iterator(); it.hasNext();)
+			super.add(it.next());
+
+		for(Iterator<FlxBlock> it = jump.iterator(); it.hasNext();)
+			super.add(it.next());
+	}
+
+	/**
+	 * PRE: Player is in the FlxState layer, height/width set.
+	 * POST: Camera focuses and follows based on level width + height
+	 */
+	private void setCameraFollow()
+	{
+		FlxG.follow(this.player, 2.5f);
+		FlxG.followAdjust(0.5f, 0.0f);
+		FlxG.followBounds(0, 0, this.width + 100, this.height + 100);
+	}
+
+	/**
+	 * Copies all mutable objects from the provided Level to this instance
+	 * @param level
+	 */
+	private final void copyContents(Level level)
+	{
+		this.player = level.player;
+		this.enemies = level.enemies;
+		this.labels = level.labels;
+		this.jump = level.jump;
+		this.movingBlocks = level.movingBlocks;
+		this.stationaryBlocks = level.stationaryBlocks;
+		this.hurtBlocks = level.hurtBlocks;
+		this.deathBlocks = level.deathBlocks;
+		this.name = level.name;
+		this.background = level.background;
+		this.width = level.width;
+		this.height = level.height;
+		this.defaultTexture = level.defaultTexture;
 	}
 }
