49d74cc94ede04b7ec200cf81b2b90b2f3ae8df8
anthony.joseph.greer <anthony.joseph.greer@9fdaa3d8-173e-11df-8630-0728b3a1dc89>
2010-04-10 16:11:52 +0000

    Made HUD more like standard FlxComponent, Made HUD text larger, finished level 1, combined all matt graphics into one image, shortened beach BG

--- branches/iteration2/SuperMattsWorld/src/org/myname/flixeldemo/Hud.java
+++ branches/iteration2/SuperMattsWorld/src/org/myname/flixeldemo/Hud.java
@@ -1,50 +1,73 @@
 package org.myname.flixeldemo;
 
+import java.text.MessageFormat;
+
 import org.flixel.FlxG;
 import org.flixel.FlxSprite;
 import org.flixel.FlxText;
+import org.myname.flixeldemo.parsing.Level;
 
 import android.graphics.Color;
 
-public class Hud extends FlxSprite
+/**
+ * Shows the level information necessary for the user including the time left in
+ * the level. If time runs out, the HUD will kill the player.
+ * 
+ * @author Roger Walker, Tony Greer
+ */
+public final class Hud extends FlxSprite
 {
-	protected FlxText hudText;
-	protected FlxSprite background;
+	/* Format String for HUD */
+	private static final String HUD_TXT_FMT = "Time: {0}     Level: {1}     x: {2}  y: {3}";
 
-	public Hud(){
-		  hudText = new FlxText((int)(-FlxG.scroll.x), (int)(-FlxG.scroll.y), (int)(FlxG.width));
+	private final FlxText hudText;
+	private final FlxSprite background;
+	private final String levelName;
 
+	public Hud(String lvlName)
+	{
+		  this.hudText = new FlxText((int)(-FlxG.scroll.x), (int)(-FlxG.scroll.y), (int)(FlxG.width));
+		  this.hudText.setSize(18);
 		  // TODO Change Font of hudText		  
-		  background = new FlxSprite((int)(-FlxG.scroll.x), (int)(-FlxG.scroll.y),
+		  this.background = new FlxSprite((int)(-FlxG.scroll.x), (int)(-FlxG.scroll.y),
 				  					null, false, (int)(FlxG.width), 50, Color.GRAY);
-		  background.setAlpha(0.4f);
+		  this.background.setAlpha(0.4f);
+		  this.levelName = lvlName;
 	}
 
-	public void updateHud(String str){
-		// Update HUD
-		// attempt to stop text bounce
+	@Override
+	public void update()
+	{
 		hudText.x =  -FlxG.scroll.x/2 * 2; 
 		hudText.y =  -FlxG.scroll.y/2 * 2;
 		hudText.width = FlxG.width;
 		background.x =  -FlxG.scroll.x/2 * 2;
 		background.y =  -FlxG.scroll.y/2 * 2;
 		background.width = FlxG.width;
-		hudText.setText(str);
+
+		// Update HUD
+		if(Level.timeRemaining > 0)
+		{
+			this.hudText.setText(MessageFormat.format(HUD_TXT_FMT,
+					(int)(Level.timeRemaining -= FlxG.elapsed), //{0}
+					this.levelName,								//{1}
+					(int)Level.player.x,						//{2}
+					(int)Level.player.y));						//{3}
+		}else if(Level.timeRemaining <= 0 && !Level.player.dead)
+		{
+			Level.player.kill();
+		}
+
+		//-- Update components
+		this.hudText.update();
+		this.background.update();
 	}
 
-	public FlxText getHudText() {
-		return hudText;
-	}
-
-	public void setHudText(FlxText hudText) {
-		this.hudText = hudText;
-	}
-
-	public FlxSprite getBackground() {
-		return background;
-	}
-
-	public void setBackground(FlxSprite background) {
-		this.background = background;
+	@Override
+	public void render()
+	{
+		//-- Render components
+		this.hudText.render();
+		this.background.render();
 	}
 }
--- branches/iteration2/SuperMattsWorld/src/org/myname/flixeldemo/JumpBlock.java
+++ branches/iteration2/SuperMattsWorld/src/org/myname/flixeldemo/JumpBlock.java
@@ -12,45 +12,43 @@ import org.myname.flixeldemo.parsing.Level;
  */
 public final class JumpBlock extends FlxBlock
 {
-        /** The label to jump to within the level specified */
-        protected final String label;
-        /** The level to which the label is contained. Cannot be changed. */
-        protected final String goToLevel;
-        /** Only hit this block once then it is ineffective. default: <tt>false</tt> */
-        protected final boolean oneTimeOnly;
+	/** The label to jump to within the level specified */
+	protected final String label;
+	/** The level to which the label is contained. Cannot be changed. */
+	protected final String goToLevel;
+	/** Only hit this block once then it is ineffective. default: <tt>false</tt> */
+	protected final boolean oneTimeOnly;
 
-        //-- Internal variable used for determining whether or
-        //   not this bad boy has been hit/used already.
-        private boolean hitAlready = false;
+	//-- Internal variable used for determining whether or
+	//   not this bad boy has been hit/used already.
+	private boolean hitAlready = false;
 
-        public JumpBlock(int X, int Y, int Width, int Height, String goToLevel, String label, boolean oneTimeOnly)
-        {
-                super(X, Y, Width, Height);
-                this.oneTimeOnly = oneTimeOnly;
-                this.goToLevel = goToLevel;
-                this.label = label;
-        }
+	public JumpBlock(int X, int Y, int Width, int Height, String goToLevel, String label, boolean oneTimeOnly)
+	{
+		super(X, Y, Width, Height);
+		this.oneTimeOnly = oneTimeOnly;
+		this.goToLevel = goToLevel;
+		this.label = label;
+	}
 
-        @Override
-        /**
-         * Checks properties of the JumpBlcok to see if we should activate a
-         * level/state switch for the jump.
-         */
-        public boolean overlaps(FlxCore Core)
-        {
-                //-- If there is an overlap, check
-                //   the instance variables and switch state if necessary
-                if(super.overlaps(Core))
-                {
-                        if(hitAlready && oneTimeOnly)
-                                return false;
-                        else
-                        {
-                                this.hitAlready = true;
-                                Level.switchLevel(this.goToLevel, this.label);
-                        }
-                }
+	@Override
+	/**
+	 * Checks properties of the JumpBlcok to see if we should activate a
+	 * level/state switch for the jump.
+	 */
+	public boolean overlaps(FlxCore Core)
+	{
+		//-- If there is an overlap, check
+		//   the instance variables and switch state if necessary
+		if(!(hitAlready && oneTimeOnly))
+		{
+			if(super.overlaps(Core))
+			{
+				this.hitAlready = true;
+				Level.switchLevel(this.goToLevel, this.label);
+			}
+		}
 
-                return false;   
-        }
+		return false;   
+	}
 }
